a263065baa598203f7bf999b1ae96ec34cf129ba
MostafaAssaff <mostafaassaaff@gmail.com>
2025-05-30 17:05:21 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-0953476d60561c955"  # Ubuntu 20.04 in us-east-1
   instance_type = "t2.micro"
-  key_name      = "tf-jen"
+  key_name      = "ansible"
 
   tags = {
     Name = "jenkins-ec2"
